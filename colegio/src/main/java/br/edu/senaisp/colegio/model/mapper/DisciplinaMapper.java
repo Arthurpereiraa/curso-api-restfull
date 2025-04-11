ddba34a29d11cdf44fcbd96c6b161a60807a8dc2
ThurDn <arthurpsf8@gmail.com>
2025-04-11 19:07:39 -0300

    Atualização do projeto

---
+++ colegio/src/main/java/br/edu/senaisp/colegio/model/mapper/DisciplinaMapper.java
@@ -0,0 +1,26 @@
+package br.edu.senaisp.colegio.model.mapper;
+
+import org.springframework.stereotype.Component;
+
+import br.edu.senaisp.colegio.model.Disciplina;
+import br.edu.senaisp.colegio.model.dto.DisciplinaDTO;
+
+@Component
+public class DisciplinaMapper {
+
+	public Disciplina toDisciplina(DisciplinaDTO dto) {
+		Disciplina tmp = new Disciplina();
+		tmp.setId(dto.id());
+		tmp.setTitulo(dto.titulo());
+		tmp.setDuracaoH(dto.duracaoH());
+		return tmp;
+	}
+	
+	public DisciplinaDTO toDisciplinaDTO(Disciplina disc) {
+		return new DisciplinaDTO(
+				disc.getId(), 
+				disc.getTitulo(), 
+				disc.getDuracaoH());
+	}	
+	
+}
